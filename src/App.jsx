310b181172e850237f37416393f74f1d7a3acc40
Sean Nam <blt24q4@gmail.com>
2024-12-20 14:39:28 +0900

    사용자 정의 props

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import StyledComponents from "/src/styled/StyledComponents";
 import PropsComponents from "/src/styled/PropsComponents";
-
+import StyledDiv from "/src/styled/UserPropsComponents";
 function App() {
   return (
     <div>
       <h1>StyledComponents</h1>
       <StyledComponents />
       <h1>PropsComponents</h1>
-      <PropsComponents />
+      {/* <PropsComponents /> */}
+      <h1>UserPropsComponents</h1>
+      <StyledDiv dark={false} />
+      <StyledDiv dark={true} />
     </div>
   );
 }
